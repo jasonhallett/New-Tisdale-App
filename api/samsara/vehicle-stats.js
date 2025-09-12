b8cfd334189ec3f46def970087f92111bfc2e73f
jasonhallett <jason.hallett@gmail.com>
2025-09-12 12:42:14 -0400

    V1

--- api/samsara/vehicle-stats.js
+++ api/samsara/vehicle-stats.js
@@ -1,4 +1,4 @@
-// /api/samsara/vehicle-stats.jsa
+// /api/samsara/vehicle-stats.js
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
